c1acf2694072d253e51c1d91032efbbe543397bd
Poonam <poonam1991gaikwad@gmail.com>
2021-12-19 00:13:47 +0900

    Updates

---
+++ precache-manifest.ea1862219fe44d9a421938d85aacd17f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8abc74d902072265e9c8",
+    "url": "/poonam-react/static/css/main.01002fe7.chunk.css"
+  },
+  {
+    "revision": "8abc74d902072265e9c8",
+    "url": "/poonam-react/static/js/main.8abc74d9.chunk.js"
+  },
+  {
+    "revision": "b6df76e426c5640b2562",
+    "url": "/poonam-react/static/js/1.b6df76e4.chunk.js"
+  },
+  {
+    "revision": "9d3d3b01a3e8c2ee3ed4",
+    "url": "/poonam-react/static/js/2.9d3d3b01.chunk.js"
+  },
+  {
+    "revision": "99dce6c007f0f486eaa3",
+    "url": "/poonam-react/static/js/runtime~main.99dce6c0.js"
+  },
+  {
+    "revision": "08fe6f450c28f5db7318e91864d4f247",
+    "url": "/poonam-react/index.html"
+  }
+];
